cdfe373b8271e106f5e92cc8cfb7dc17393370fe
Brayan Alexander Yepes Clavijo <abrayan.claye@gmail.com>
2022-09-04 10:19:34 -0500

    listItem component

--- src/app/components/ListItem.jsx
+++ src/app/components/ListItem.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { getListData, removeReservation } from "../services/localstorage";
 
-const ListItem = () => {
-  return <div>ListItem</div>;
+const ListItem = ({ reservation, setReservations }) => {
+  const { uuid, origin, destination, passengers, date } = reservation;
+  const navigate = useNavigate();
+  const deleteReservation = () => {
+    removeReservation(uuid);
+    setReservations(getListData());
+  };
+  return (
+    <div
+      key={uuid}
+      className="bg-gray-200 m-2 p-2 flex justify-between items-center rounded"
+    >
+      <div className="flex flex-col">
+        <p className="text-center">Ticket N°</p>
+        <span className="text-sm">{uuid}</span>
+        <button
+          className="p-2 bg-indigo-500 rounded text-white my-1"
+          onClick={() => navigate(`/details/${uuid}`)}
+        >
+          Ver detalles
+        </button>
+      </div>
+      <div>
+        <p className="flex text-amber-500">
+          Origen: <p className="mx-4 text-black">{origin}</p>
+        </p>
+        <p className="flex text-amber-500">
+          Destino: <p className="mx-4 text-black">{destination}</p>
+        </p>
+        <p className="flex text-amber-500">
+          N° Pasajeros: <p className="mx-4 text-black">{passengers}</p>
+        </p>
+        <p className="flex text-amber-500">
+          Fecha: <p className="mx-4 text-black">{date}</p>
+        </p>
+      </div>
+      <div className="flex flex-col">
+        <button
+          onClick={() => navigate(`/form/${uuid}`)}
+          className="p-2 bg-amber-500 rounded text-white my-1"
+        >
+          Editar
+        </button>
+        <button
+          onClick={() => deleteReservation()}
+          className="p-2 bg-red-500 rounded text-white my-1"
+        >
+          Eliminar
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ListItem;
