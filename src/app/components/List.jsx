ff2a880a419c13c10f7a2e1ddd3edf7ed7ef8557
Brayan Alexander Yepes Clavijo <abrayan.claye@gmail.com>
2022-09-04 10:17:28 -0500

    list component

--- src/app/components/List.jsx
+++ src/app/components/List.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getListData } from "../services/localstorage";
+import ListItem from "./ListItem";
+import { useNavigate } from "react-router-dom";
 
 const List = () => {
-  return <div>List</div>;
+  const [reservations, setReservations] = useState([]);
+  useEffect(() => {
+    setReservations(getListData);
+  }, []);
+  const navigate = useNavigate();
+  return (
+    <div className="">
+      {reservations.map((reservation) => (
+        <ListItem reservation={reservation} setReservations={setReservations} />
+      ))}
+      {reservations.length === 0 && (
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-center text-xl mt-40">No hay ninguna reserva</h1>
+          <button
+            className="bg-amber-500 text-white p-2 rounded m-4 hover:bg-indigo-500 transition-all"
+            onClick={() => navigate("/form")}
+          >
+            Quiero hacer una reservación
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default List;
