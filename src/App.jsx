63d5f048b26cdf57f7d6512bffa495c64552f351
Brayan Alexander Yepes Clavijo <abrayan.claye@gmail.com>
2022-09-04 09:42:32 -0500

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Details from "./app/components/Details";
+import Footer from "./app/components/Footer";
+import Form from "./app/components/Form";
+import List from "./app/components/List";
+import Navbar from "./app/components/Navbar";
+
 function App() {
-  return <div className="App">Hola mundo!</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<List />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/form/:uuid" element={<Form />} />
+        <Route path="/details/:uuid" element={<Details />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
