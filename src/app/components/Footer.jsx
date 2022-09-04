d101cb734cd9a5020b560aff22d633d07bc50e46
Brayan Alexander Yepes Clavijo <abrayan.claye@gmail.com>
2022-09-04 09:46:11 -0500

    navbar and footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <nav className="fixed bottom-0 bg-indigo-500 w-full flex justify-center p-2 text-white">
+      <p>
+        Hecho por{" "}
+        <a
+          href="https://www.linkedin.com/in/brayan-yepesc/"
+          className="text-amber-500 font-bold"
+        >
+          Brayan Yepes
+        </a>
+      </p>
+    </nav>
+  );
 };
 
 export default Footer;
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,7 +1,27 @@
+import { useNavigate } from "react-router-dom";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+  return (
+    <nav className="sticky top-0 flex justify-between bg-indigo-500 p-2 items-center">
+      <div className="text-white">Bookingapp</div>
+      <div>
+        <button
+          className="mx-2 bg-indigo-700 rounded text-white p-2 hover:bg-amber-500 transition-all"
+          onClick={() => navigate("/")}
+        >
+          Reservaciones
+        </button>
+        <button
+          className="mx-2 bg-indigo-700 rounded text-white p-2 hover:bg-amber-500 transition-all"
+          onClick={() => navigate("/form")}
+        >
+          Reservar
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
